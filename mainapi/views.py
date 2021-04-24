4853a408fe2dc640f5efa008f8b09c82a01d122b
YashKandalkar <yashsk.yk@gmail.com>
2021-04-24 17:06:31 +0530

    feisfe:

--- mainapi/views.py
+++ mainapi/views.py
@@ -5,28 +5,31 @@ from urllib.parse import unquote, urlparse, parse_qs
 import pymongo
 
 # MongoDB client
-client = pymongo.MongoClient("mongodb+srv://adminuser:adminuserpassword@cluster0.tilcf.mongodb.net/myDB2?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://adminuser:adminuserpassword@cluster0.tilcf.mongodb.net/myDB2?retryWrites=true&w=majority")
 
 # Database Name
-db = client["myDB"]
+db = client["myDB3"]
 
 # Collection
-col = db.myCollection
+col = db.collection5
 
 col.create_index([
     ("url", "text"), ("text", "text")
 ])
 
+
 def displaySearch(input_string):
 
     s = col.find(
         {"$text": {"$search": 'i want some help"'}},
-        {'score': {'$meta': 'textScore'}, '_id' : 0 }
+        {'score': {'$meta': 'textScore'}, '_id': 0}
     )
     s.sort([('score', {'$meta': 'textScore'})])
     s = list(s)
     s = s[:15]
     return s
+
 
 def search(request):
     s = request.get_raw_uri()
@@ -34,7 +37,7 @@ def search(request):
     text = unquote(text)
 
     responseData = {
-        'result' : displaySearch(text)
+        'result': displaySearch(text)
     }
 
     return JsonResponse(responseData)
